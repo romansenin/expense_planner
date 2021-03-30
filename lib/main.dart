11b09a9e6138d5a5ed91b7b3c507453ca7d53cfd
Roman Senin <roman@launchbadge.com>
2021-03-30 12:21:31 -0700

    add TextField widgets

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class MyHomePage extends StatelessWidget {
           title: Text('Flutter App'),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          // mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Card(
@@ -48,6 +48,32 @@ class MyHomePage extends StatelessWidget {
                 child: Text("CHART!"),
               ),
               elevation: 5,
+            ),
+            Card(
+              elevation: 5,
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: "Title",
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: "Amount",
+                      ),
+                    ),
+                    FlatButton(
+                      child: Text("Add Transaction"),
+                      textColor: Colors.purple,
+                      onPressed: () {},
+                    )
+                  ],
+                ),
+              ),
             ),
             Column(
               children: transactions
