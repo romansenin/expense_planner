515738a575280d29d38a9f1570349dd3be3ea620
Roman Senin <roman@launchbadge.com>
2021-03-30 11:48:45 -0700

    finish transaction styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import './transaction.dart';
 
@@ -61,19 +62,36 @@ class MyHomePage extends StatelessWidget {
                             ),
                             decoration: BoxDecoration(
                               border: Border.all(
-                                color: Colors.black,
+                                color: Colors.purple,
                                 width: 2,
                               ),
                             ),
                             padding: EdgeInsets.all(10),
                             child: Text(
-                              tx.amount.toString(),
+                              "\$${tx.amount}",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Colors.purple,
+                              ),
                             ),
                           ),
                           Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Text(tx.title),
-                              Text(tx.date.toIso8601String())
+                              Text(
+                                tx.title,
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                DateFormat.yMMMd().format(tx.date),
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                ),
+                              )
                             ],
                           )
                         ],
